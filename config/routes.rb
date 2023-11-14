ec2aaab2bba6845a85c863b4f34d4fd38bf21644
Pedro Braga Miranda <pbragamiranda@pm.me>
2023-11-14 11:21:45 +0100

    Crud Actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # resources :restaurants, only: [:index, :show, :new, :create]
+  resources :restaurants
+
+  # # See all restaurants
+  # get "restaurants", to: "restaurants#index"
+  # # Create a restaurant
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # # See details one restaurant
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # # Update a restaurant
+  # get "restaurants/:id/edit", to: "restaurants#edit" #, as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+  # # Destroy a restaurant
+  # delete "restaurants/:id", to: "restaurants#destroy"
 end
